f6ad68033e95023a7b273448aa59873fc68ada22
Arthur Gerhardt <renewedjesus@gmail.com>
2025-07-08 11:35:00 -0300

    Avançando na aula.

--- app1.py
+++ app1.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
+
+# Configurações da Aplicação
+
 app = Flask(__name__)
+
+app. config.from_object ('config')
+
+print(app.config['SECRET_KEY'])
+print (app.config['SQLALCHEMY_DATABASE_URI'])
+print (app.config ['SWAGGER' ])
+print (app. config ['CACHE_TYPE' ])
 
 @app.route('/')
 def main():
