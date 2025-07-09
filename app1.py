479649c65712fd53416d72fd27f346b4f6a9d15b
Arthur Gerhardt <renewedjesus@gmail.com>
2025-07-09 16:49:23 -0300

    Aprendendo mais sobre SQLAlchemy em ML.

--- app1.py
+++ app1.py
@@ -1,16 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Certifique-se de que o arquivo config.py existe ou defina a classe Config abaixo
+# from config import Config
 
 # Configurações da Aplicação
 
 app = Flask(__name__)
 
-app. config.from_object ('config')
+app.config.from_object('config')  # ou 'Config' se você importar uma classe Config
 
-print(app.config['SECRET_KEY'])
-print (app.config['SQLALCHEMY_DATABASE_URI'])
-print (app.config ['SWAGGER' ])
-print (app. config ['CACHE_TYPE' ])
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Banco de dados criado!")
+
+print(app.config.get('SECRET_KEY'))
+print(app.config.get('SQLALCHEMY_DATABASE_URI'))
+print(app.config.get('SWAGGER'))
+print(app.config.get('CACHE_TYPE'))
 
 @app.route('/')
 def main():
--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 class Config:
-    SECRET_KEY = 'sua_chave_secreta'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'sua_chave_secreta')
     CACHE_TYPE = 'simple'
     SWAGGER = {
         'title': 'Catálogo de Receitas Gourmet',
         'uiversion': 3
     }
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///recipes.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///recipes.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'sua_chave_jwt_secreta'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'sua_chave_jwt_secreta')
