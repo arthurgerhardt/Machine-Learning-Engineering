e4d5441f9505e8569d356602f62e5be2bee1d73c
Arthur Gerhardt <renewedjesus@gmail.com>
2025-07-16 17:35:05 -0300

    Ajustando o código novamente.

--- Aula02/app.py
+++ Aula02/app.py
@@ -1,37 +1,26 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
-
-app.config.from_object('config.Config')
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-
-print(app.config['SECRET_KEY'])
-print(app.config['SQLALCHEMY_DATABASE_URI'])
-print(app.config['SWAGGER'])
-print(app.config['CACHE_TYPE'])
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    ingredients = db.Column(db.Text, nullable=False)
-    time_minutes = db.Column(db.Integer, nullable=False)
+@app.route('/')
+def home():
+    return "Welcome to the Flask App!"
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Banco de dados criado!")
-
-@app.route('/')
-def main():
-    return "Hello, Flask + SQLite!"
-
-if __name__ == '__main__':
-    
+        print(app.config['SECRET_KEY'])
+        print(app.config['SQLALCHEMY_DATABASE_URI'])
+        print(app.config['SWAGGER'])
+        print(app.config['CACHE_TYPE'])
     app.run(debug=True)
--- Aula02/config.py
+++ Aula02/config.py
@@ -2,9 +2,9 @@ class Config:
     SECRET_KEY = 'sua_chave_secreta'
     CACHE_TYPE = 'simple'
     SWAGGER = {
-        'title': 'Catalogo de Receitas Gourmet',
+        'title': 'Catálogo de Receitas Gourmet',
         'uiversion': 3
     }
     SQLALCHEMY_DATABASE_URI = 'sqlite:///recipes.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'sua_chave_jwt_secreta'.format()
+    JWT_SECRET_KEY = 'sua_chave_jwt_secreta'
