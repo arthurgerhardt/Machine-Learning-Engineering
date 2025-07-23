7195b45a9e39bf58eaefe9e754da5e8934bf466f
Arthur Gerhardt <renewedjesus@gmail.com>
2025-07-23 09:56:25 -0300

    Ajuste final de código!

--- Aula03/app.py
+++ Aula03/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required, get_jwt_identity
+    JWTManager, jwt_required, create_access_token, get_jwt_identity
 )
 
 app = Flask(__name__)
-db = SQLAlchemy(app)  
-jwt = JWTManager(app)  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,21 +16,65 @@ class User(db.Model):
 
 @app.route('/register', methods=['POST'])
 def register_user():
-    # Register a new user.
+    """ 
+    Registra um novo usuário.
+     Registra um novo usuário.
+     parameters:
+        - In: body
+        name: body 
+        required: true 
+        schema:
+          type: object
+          properties:
+            username:
+              type: string
+            password:
+              type: string
+          responses:
+            201:
+              description: Usuário criado com sucesso
+            400:
+              description: Usuário já existe
+    """
     data = request.get_json()
     if User.query.filter_by(username=data['username']).first():
         return jsonify({"error": "User already exists"}), 400
-# Removed invalid block. The registration endpoint is already defined above.
+    new_user = User(username=data['username'], password=data['password'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"msg": "User created"}), 201
 
+@app.route('/login', methods=['POST'])
+def login():
+    """
+    Faz login do usuário e retorna um JWT.
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            username:
+              type: string
+            password:
+              type: string
+    responses:
+      200:
+        description: Login bem sucedido, retorna JWT
+      401:
+        description: Credenciais inválidas
+    """
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.password == data['password']:
+        # Converter o ID para string
+        token = create_access_token(identity=str(user.id))
+        return jsonify({"access_token": token}), 200
+    return jsonify({"error": "Invalid credentials"}), 401
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        print("Database created!")
     app.run(debug=True)
-
-if __name__ == '__main__':
-    
-    app.run(debug=True)
-                                
